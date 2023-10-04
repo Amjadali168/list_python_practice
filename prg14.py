9424e58cd57f7f93e8a3f89338229669e60a6fa0
Amjadali168 <amjadali1682002@gmail.com>
2023-10-04 21:00:42 +0530

    list day 4

---
+++ prg14.py
@@ -0,0 +1,7 @@
+# Write a program that creates a list of integers and then calculates the average of all the numbers in the list. Print the average.
+integer_list = [5, 10, 15, 20, 25]
+
+total_sum = sum(integer_list)
+
+average = total_sum / len(integer_list)
+print("Average:", average)
