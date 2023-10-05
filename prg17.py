b2527cf7bb2ede5b1abeeadfd981824af018464a
Amjadali168 <amjadali1682002@gmail.com>
2023-10-05 21:24:58 +0530

    list day 5

---
+++ prg17.py
@@ -0,0 +1,4 @@
+# Write a program that creates a list of integers and then sorts the list in descending order. Print the sorted list.
+integer_list = [10, 5, 8, 13, 2, 7]
+sorted_list = sorted(integer_list, reverse=True)
+print(sorted_list)
